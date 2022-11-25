e3b6cbbf23fac9559753f1c4b12b3a07df217ad2
Aditya Raj Singh <adityaraj423582@gmail.com>
2022-11-26 02:32:50 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,86 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: '    TITAN',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-05.png',
+    title: 'CYBERTON',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'VOLMIR',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-03.png',
+    title: 'DARKSIDE',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading ALCAZERZ to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+ 
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The metaverse is a hypothetical iteration of the Internet as a single, universal and immersive virtual world',
+   
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'The launch of the Metaverse makes Mark Zuckerberg ketar-ketir',
+    
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'The metaverse will change the way we perceive user data',
+    
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
